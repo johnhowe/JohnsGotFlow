c9d81c639f95ad8b4ab778f6a8e0829e53df3d50
John Howe <johnhowe@gmail.com>
2012-06-21 00:16:31 +1200

    Not using ADC

--- config.h
+++ config.h
@@ -4,9 +4,6 @@
 #define REFRESH_HZ              50      // Main loop frequency
 #define STARTUP_DELAY           300     // Milliseconds for the device to allow other peripherals to startup
 
-/* GPIO Pins */
-#define ACCEL_PWR               INCH_0  // Accelerometer power output
-#define ACCEL_ADC               INCH_7  // Accelerometer analogue input
 /* SPI pins */
 #define MAX31855_CS_PIN         BIT2    // Chip select for thermocouple IC
 #define DISPLAY_CS_PIN          BIT3    // Chip select for display
--- main.c
+++ main.c
@@ -77,10 +77,7 @@ void initialise(void)
 
         /* Initialise I/O ports */
         P1OUT = 0;
-        P1DIR |= ( DISPLAY_CS_PIN | MAX31855_CS_PIN | MOSI_PIN | CLK_PIN | ACCEL_PWR ); // Set output pins
-        P1DIR &= ~( ACCEL_ADC ); // Set input pins
-        ADC10AE0 |= ACCEL_ADC; // Enable ADC
-        P1OUT |= ACCEL_PWR; // Power on the accelerometer and display
+        P1DIR |= ( DISPLAY_CS_PIN | MAX31855_CS_PIN | MOSI_PIN | CLK_PIN ); // Set output pins
 
         _BIS_SR(GIE); // Global interrupt enable
 
